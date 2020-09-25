244d7166bb1ad0862ea1aeaf0adc85a57229722d
Bibidi <>
2020-09-25 13:36:09 +0900

    no message

---
+++ Atcoder/ABC/A/174.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int x; cin >> x;
+    
+    if (x >= 30) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
