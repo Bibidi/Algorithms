694ee3cf9c85c9f77ff9d650e50185bfb253d3f0
Bibidi <byungdeuk.kim@gmail.com>
2023-02-13 23:16:00 +0900

    Fast input & Fast output code

---
+++ libraries/FastInputOutput.java
@@ -0,0 +1,89 @@
+
+public class FastInputOutput {
+    static class FastReader {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            if (st != null && st.hasMoreTokens()) {
+                return st.nextToken();
+            }
+
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            if (st != null && st.hasMoreTokens()) {
+                return st.nextToken("\n");
+            }
+
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) {
+            try {
+                bw.append(object.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void println(Object object) {
+            try {
+                bw.append(object.toString()).append("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void close() {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+
+        out.close();
+    }
+}
