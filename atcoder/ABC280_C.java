d33dbc607fb8aa47685e79ed0df72bfb37cfa09e
Bibidi <byungdeuk.kim@gmail.com>
2023-02-23 23:21:59 +0900

    solve 1 atcoder problems

---
+++ atcoder/ABC280_C.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.*;
+
+public class ABC280_C {
+    static class FastReader {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            if (st != null && st.hasMoreTokens()) {
+                return st.nextToken();
+            }
+
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            if (st != null && st.hasMoreTokens()) {
+                return st.nextToken("\n");
+            }
+
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) {
+            try {
+                bw.append(object.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void println(Object object) {
+            try {
+                bw.append(object.toString()).append("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void close() {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+
+        String s = in.next();
+        String t = in.next();
+
+        for (int i = 0; i < t.length(); i++) {
+            if (i >= s.length()) {
+                out.println(i + 1);
+                break;
+            }
+            if (s.charAt(i) != t.charAt(i)) {
+                out.println(i + 1);
+                break;
+            }
+        }
+
+        out.close();
+    }
+}
